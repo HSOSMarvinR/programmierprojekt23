2b0e77747546c85500fb4f09a724d29d8e084bad
Sebsch08 <sebastian.scheipers@hs-osnabrueck.de>
2023-09-19 08:44:36 +0200

    first_mockup_S

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderMenueComponent } from './header-menue/header-menue.component';
+import { DataMenueComponent } from './data-menue/data-menue.component';
+import { VisualizeComponent } from './visualize/visualize.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderMenueComponent,
+    DataMenueComponent,
+    VisualizeComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/data-menue/data-menue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-menue',
+  templateUrl: './data-menue.component.html',
+  styleUrls: ['./data-menue.component.css']
+})
+export class DataMenueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
