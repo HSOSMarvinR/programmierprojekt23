684b27da2f10d7d36e8f1ae22868954daf1d19d0
sebsch08 <sebastian.scheipers@hs-osnabrueck.de>
2023-10-04 14:34:07 +0200

    changes_S

--- src/app/info-panel/info-panel.component.ts
+++ src/app/info-panel/info-panel.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem, MessageService } from 'primeng/api';
 import { FormControl, FormGroup } from '@angular/forms';
+import { HttpClient } from "@angular/common/http";  //Http Client für Api
 
 
 
